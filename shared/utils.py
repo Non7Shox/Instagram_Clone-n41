6a58c227eff5c75719390dabc0bd7e05dbf66679
Non7Shox <muhammadsodiqkasimov34@gmail.com>
2024-06-28 18:41:50 +0500

    third commit

--- shared/utils.py
+++ shared/utils.py
@@ -24,8 +24,8 @@ def send_code_to_email(email, code):
 
 def send_code_to_phone(phone_number, code):
     def send_in_thread():
-        account_sid = config('TWILIO_ID')
-        auth_token = config('TWILIO_KEY')
+        account_sid = 'AC35e4b4b1654fc26354adab63e12ca90a'
+        auth_token = '4f884ad66e553812b27cdbb903324f4a'
         client = Client(account_sid, auth_token)
 
         verification_check = client.verify \
@@ -33,8 +33,6 @@ def send_code_to_phone(phone_number, code):
             .services('VA9060063ba3efe707a8ca8e9c37f29e9d') \
             .verification_checks \
             .create(to='+998900355948', code='[Code]')
-
-        print(verification_check.status)
 
     thread = threading.Thread(target=send_in_thread)
     thread.start()
